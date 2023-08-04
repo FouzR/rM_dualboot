2e32a7ebccfab23c5bf99dbe9b70ea67543828ab
FouzR <88454554+FouzR@users.noreply.github.com>
2023-08-05 00:11:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 wget -O - -nc https://raw.githubusercontent.com/ddvk/remarkable-update/main/switch.sh
+chmod +x ./switch.sh
 wget -O switch_service.service https://raw.githubusercontent.com/FouzR/rM_dualboot/main/switch_service.service
 cp switch_service.service /etc/systemd/system/
 chmod 644 /etc/systemd/system/switch_service.service
