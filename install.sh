87cb23dff9f53aad191369eb1b5bd1cdaa5aa55d
FouzR <88454554+FouzR@users.noreply.github.com>
2023-08-07 23:20:18 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ wget -O switch.sh -nc https://raw.githubusercontent.com/ddvk/remarkable-update/m
 wget -O switch_service.service https://raw.githubusercontent.com/FouzR/rM_dualboot/main/switch_service.service
 declare -A checksums=(
 ["./switch.sh"]="c6b165745d67cb7adc62d7826253ad027a55ee2551d189c37f7d3181e7358044"
-["./switch_service.service"]="a01347e73266828df253a69b13c71c0e65e386a5bb339a02a304558c15d5f4b4"
+["./switch_service.service"]="68511f046e4d5ea4fd97314e7c7451279aeae232d3ea338f0d275cc48175253d"
 )
 for check in ${!checksums[@]} 
 do
