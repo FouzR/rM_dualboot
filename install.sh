67043e4e679fec82125930099ca2a179436edfa1
FouzR <88454554+FouzR@users.noreply.github.com>
2023-08-21 17:57:51 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 wget -O switch.sh -nc https://raw.githubusercontent.com/ddvk/remarkable-update/main/switch.sh
 wget -O switch_service.service https://raw.githubusercontent.com/FouzR/rM_dualboot/main/switch_service.service
+wget -O suspended.png https://raw.githubusercontent.com/FouzR/rM_dualboot/WithSuspended.png/suspended.png
 declare -A checksums=(
 ["./switch.sh"]="c6b165745d67cb7adc62d7826253ad027a55ee2551d189c37f7d3181e7358044"
 ["./switch_service.service"]="8ac9b202330e4a57d8b2b7a0cdb938f29fed118be395732f960693ee81ab027e"
@@ -16,6 +17,7 @@ do
 done
 chmod +x ./switch.sh
 cp switch_service.service /etc/systemd/system/
+cp suspended.png /usr/share/remarkable
 chmod 644 /etc/systemd/system/switch_service.service
 systemctl daemon-reload
 systemctl enable --now switch_service.service
