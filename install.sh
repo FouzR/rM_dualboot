d991fe1c66d74ab77761c921c24a1d0c2c6eba0e
FouzR <fouz290903@gmail.com>
2023-09-23 10:20:09 +0400

    Added code to add service to other partition in one go

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 wget -O switch.sh -nc https://raw.githubusercontent.com/ddvk/remarkable-update/main/switch.sh
 wget -O switch_service.service https://raw.githubusercontent.com/FouzR/rM_dualboot/main/switch_service.service
-wget -O suspended.png https://raw.githubusercontent.com/FouzR/rM_dualboot/WithSuspended.png/suspended.png
 declare -A checksums=(
 ["./switch.sh"]="c6b165745d67cb7adc62d7826253ad027a55ee2551d189c37f7d3181e7358044"
 ["./switch_service.service"]="8ac9b202330e4a57d8b2b7a0cdb938f29fed118be395732f960693ee81ab027e"
-["./suspended.png"]="66be2cb5cd2159e93843ce24bf1b6a11c372b7fcf9d59cbfeb77508c751156a6"
 )
 for check in ${!checksums[@]} 
 do
@@ -22,4 +20,22 @@ cp suspended.png /usr/share/remarkable
 chmod 644 /etc/systemd/system/switch_service.service
 systemctl daemon-reload
 systemctl enable --now switch_service.service
+
+# The below code aims to do the same but for the other partition
+
+# check active partition using print_env or whatever
+
+OLDPART=$(fw_printenv -n active_partition)
+if [ $OLDPART  ==  "2" ]; then
+    NEWPART="3"
+else
+    NEWPART="2"
+fi
+mkdir /mnt/old_part
+mount /dev/mmcblk2p2${OLDPART} /mnt/old_part
+cp ~/switch_service /mnt/old_part/etc/systemd/system/
+ln -s /mnt/old_part/systemd/system/switch_service.service ./multi-user.target.wants/switch_service.service
+umount /mnt/old_part
+rm -r /mnt/old_part
+
 rm switch_service.service
