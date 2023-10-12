c64cb9492821cec30cf38386f5070fe5e3537a30
FouzR <fouz290903@gmail.com>
2023-10-12 12:05:56 +0000

    Bunch of logs and added Chronic to clean err outputs

--- install.sh
+++ install.sh
@@ -1,22 +1,25 @@
 set -e
+
+if ! command chronic &> /dev/null;then
+    function chronic(){
+        tmp=$(mktemp) || return
+        "$@" > "$tmp" 2>&1
+        ret=$?
+        [ "$ret" -eq 0 ] || cat "$tmp"
+        rm -f "$tmp"
+        return "$ret"
+    }
+fi
+echo "Creating /tmp/rM_dualboot"
 mkdir /tmp/rM_dualboot || true
-wget -O switch.sh -r https://raw.githubusercontent.com/ddvk/remarkable-update/main/switch.sh
-wget -O /tmp/rM_dualboot/switch_service.service https://raw.githubusercontent.com/FouzR/rM_dualboot/main/switch_service.service
+echo "Retrieving files......"
+chronic wget -O switch.sh -r https://raw.githubusercontent.com/ddvk/remarkable-update/main/switch.sh
+chronic wget -O /tmp/rM_dualboot/switch_service.service https://raw.githubusercontent.com/FouzR/rM_dualboot/main/switch_service.service
+echo "Retrieved switch.sh and the service files"
 declare -A checksums=(
 ["./switch.sh"]="c6b165745d67cb7adc62d7826253ad027a55ee2551d189c37f7d3181e7358044"
 ["/tmp/rM_dualboot/switch_service.service"]="8ac9b202330e4a57d8b2b7a0cdb938f29fed118be395732f960693ee81ab027e"
-["./suspended.png"]="66be2cb5cd2159e93843ce24bf1b6a11c372b7fcf9d59cbfeb77508c751156a6"
 )
-
-# find device model
-Model=$(< /sys/devices/soc0/machine)
-rM2="reMarkable 2.0"
-if [[ "$Model" == "$rM2" ]]; then
-    dsk="2"
-else
-    dsk="1"
-fi
-
 for check in ${!checksums[@]}
 do
     echo "${checksums[$check]}  $check" | sha256sum -c
@@ -28,7 +31,21 @@ do
     fi
 done
 chmod +x ./switch.sh
+
+# find device model
+Model=$(< /sys/devices/soc0/machine)
+rM2="reMarkable 2.0"
+if [[ "$Model" == "$rM2" ]]; then
+    dsk="2"
+    echo "Detected rM 2"
+else
+    dsk="1"
+    echo "Detected rM 1"
+fi
+
 chmod 644 /tmp/rM_dualboot/switch_service.service
+
+echo "Copying service file....."
 cp /tmp/rM_dualboot/switch_service.service /etc/systemd/system/
 
 # check active partition using print_env or whatever
@@ -39,21 +56,38 @@ if [ $OLDPART  ==  "2" ]; then
 else
     NEWPART="2"
 fi
+
+echo "Creating /mnt/old_part"
 mkdir /mnt/old_part || true
 
 #running the partition check _before_ the systemctl runs, as that alters
 # the active_partiton, annoyingly
 
 systemctl daemon-reload
-systemctl enable --now switch_service.service
+chronic systemctl enable --now switch_service.service
+echo "Successfully enabled the service"
 # The below code aims to do the same but for the other partition
 
+echo "Now installing to secondary partition"
 
-
+echo "Mounting /dev/mmcblk${dsk}p${NEWPART} to /mnt/old_part"
 mount /dev/mmcblk${dsk}p${NEWPART} /mnt/old_part
+
+echo "Copying service file to fallback"
 cp /tmp/rM_dualboot/switch_service.service /mnt/old_part/etc/systemd/system/
+echo "Linking the service file...."
 ln -s /etc/systemd/system/switch_service.service /mnt/old_part/etc/systemd/system/multi-user.target.wants/switch_service.service
 
+fallback=$(cat /usr/share/remarkable/update.conf | grep "REMARKABLE_RELEASE_VERSION")
+current=$(cat /mnt/old_part/usr/share/remarkable/update.conf | grep "REMARKABLE_RELEASE_VERSION")
+
+fallbackV=${fallback##=}
+currentV=${current##=}
+echo "Finished installing... Unmounting the partition and cleaning up...."
 umount /mnt/old_part
 rmdir /mnt/old_part
 
+echo "Current version = ${currentV}"
+echo "Fallback version = ${fallbackV}"
+echo "Dual Booting enabled between the above two versions"
+echo "Remember to disable auto updates""
